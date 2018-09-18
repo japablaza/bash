6bd8f00d981ab023538b7789ca6c95dd32d8a34a
Jose Apablaza <japablaza@gmail.com>
2018-09-17 21:21:05 -0500

    Adding create_file with IF statements

--- create_file.sh
+++ create_file.sh
@@ -10,18 +10,28 @@ if [[ ! $1 ]]; then
 fi
 
 # Creando variables
-
-scriptdir="bin"
-filename="$scriptdir/$1"
+scriptname="$1"
+scriptdir="/bin"
+filename="${scriptdir}/${scriptname}"
 
 # Verificar si el archivo exciste
-
-if [[ -e $scriptdir ]]; then
+if [[ -e $filename ]]; then
   echo "El archivo $filename ya exciste"
   exit 1
 fi
 
-# Verificar si el directorio exciste
+# Verificar si el nombre del archivo es un comando
+if type "$scriptname"; then
+  echo "El argumento usado ${scriptname} es un comando de Linux"
+  exit 1
+fi
 
-if [[ ! -d $scriptdir]]; then
-  if [[ mkdir $scriptdir ]]
+# Verificar si el directorio exciste
+if [[ ! -d $scriptdir ]]; then
+  if mkdir "$scriptdir"; then
+    echo "Se creo el directorio ${scriptdir}"
+  else
+    "No se pudo crear el directorio ${scriptdir}"
+    exit 1
+  fi
+fi
